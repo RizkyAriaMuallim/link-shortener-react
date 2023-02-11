71ef9ce76a9cc6c61fe92d76fd1708e5ee5da652
Rizky Aria Mu'allim <74603462+RizkyAriaMuallim@users.noreply.github.com>
2023-02-11 16:40:47 +0700

    finish do logic for the application

--- src/components/UrlShortener.jsx
+++ src/components/UrlShortener.jsx
@@ -1,33 +1,55 @@
 import { useState, useEffect } from "react";
+import CopyToClipboard from "react-copy-to-clipboard";
 
 
 function UrlShortener({dataInput}) {
     const [newLink, setNewLink] = useState("");
+    const [status, setStatus] = useState(false);
+    const [copyStatus, setCopyStatus] = useState(false);
 
     const getDataAPI = async () => {
         try {
             const res = await fetch(
                 `https://api.shrtco.de/v2/shorten?url=${dataInput}`
             )
+            // API Feed-Back
             const value = await res.json();
-            console.log(value);
+            // Destruction from above
             const {result} = value;
             const {short_link} = result;
+            // new link
             setNewLink(short_link);
         } catch (error){
 
         }
         
     }
-
+    // Do get data just once
     useEffect(()=> {
         if(dataInput.length){
+            setStatus(true);
             getDataAPI();
         }
     }, [dataInput])
 
+    if(status){
+        return (
+            <>
+                <h3>{`New link is : ${newLink}`}</h3>
+                <CopyToClipboard text={newLink} onCopy={()=> setCopyStatus(true)}>
+                    <button>{copyStatus ? "Copied!!!" : "Copy"}</button>
+                </CopyToClipboard>
+            </>
+        )
+    }else{
+        return (
+            <h3>Waiting data...</h3>
+
+        )
+    }
+
     return (
-        <h3>{newLink}</h3>
+        <p></p>
     )
 }
 
